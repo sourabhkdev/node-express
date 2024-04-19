dcf2bb5e171a1c7b84d61dceb3c65231d7896e36
Sourabh Kulkarni <sourabhsk.kulkarni@gmail.com>
2024-04-19 10:30:09 +0530

    combine matching routes

--- contact-app-backend/routes/contactRoutes.js
+++ contact-app-backend/routes/contactRoutes.js
@@ -9,19 +9,27 @@ const {
 
 const router = express.Router();
 
-//GET OCNTACTE ROUTE
-router.route("/").get(getContact);
+// //GET OCNTACTE ROUTE
+// router.route("/").get(getContact);
 
-//POST OCNTACTE ROUTE
-router.route("/").post(createContact);
+// //POST OCNTACTE ROUTE
+// router.route("/").post(createContact);
 
-//GET CONTACT BY ID
-router.route("/:id").get(getContactById);
+// //GET CONTACT BY ID
+// router.route("/:id").get(getContactById);
 
-//UPDATE OCNTACTE ROUTE
-router.route("/:id").put(updateContact);
+// //UPDATE OCNTACTE ROUTE
+// router.route("/:id").put(updateContact);
 
-//DELETE OCNTACTE ROUTE
-router.route("/:id").delete(deleteContact);
+// //DELETE OCNTACTE ROUTE
+// router.route("/:id").delete(deleteContact);
+
+// combine all matching route
+router.route("/").get(getContact).post(createContact);
+router
+  .route("/:id")
+  .get(getContactById)
+  .put(updateContact)
+  .delete(deleteContact);
 
 module.exports = router;
