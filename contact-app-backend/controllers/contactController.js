575fa01941f94d26163e16ff8aac0c7a14db4659
Sourabh Kulkarni <sourabhsk.kulkarni@gmail.com>
2024-04-19 11:42:39 +0530

    express-async-handler

--- contact-app-backend/controllers/contactController.js
+++ contact-app-backend/controllers/contactController.js
@@ -1,16 +1,23 @@
+const asyncHandler = require("express-async-handler"); // use express-async-handler to eliminate use of try catch
+
 //@desc Get all contacts
 //@route GET /api/contacts
 //@access public
 
-const getContact = (req, res) => {
+{
+  /* while interacting with MOngo db we always get promise and to resolve that promise,
+here we will use AYNC AWAIT*/
+}
+
+const getContact = asyncHandler(async (req, res) => {
   res.status(200).json({ message: "Get all contacts" });
-};
+});
 
 //@desc Create new contacts
 //@route POST /api/contacts
 //@access public
 
-const createContact = (req, res) => {
+const createContact = asyncHandler(async (req, res) => {
   console.log("The request body is", req.body);
   const { name, email, phone } = req.body;
   if (!name || !email || !phone) {
@@ -18,31 +25,31 @@ const createContact = (req, res) => {
     throw new Error("All fields mandatory");
   }
   res.status(200).json({ message: "Create contact" });
-};
+});
 
 //@desc Get contact by id
 //@route GET /api/contacts/:id
 //@access public
 
-const getContactById = (req, res) => {
+const getContactById = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `Get contact for ${req.params.id}` });
-};
+});
 
 //@desc Update contact by id
 //@route PUT /api/contacts/:id
 //@access public
 
-const updateContact = (req, res) => {
+const updateContact = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `Update contact for ${req.params.id}` });
-};
+});
 
 //@desc delte contact by id
 //@route DELETE /api/contacts/:id
 //@access public
 
-const deleteContact = (req, res) => {
+const deleteContact = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `Delete contact for ${req.params.id}` });
-};
+});
 
 module.exports = {
   getContact,
