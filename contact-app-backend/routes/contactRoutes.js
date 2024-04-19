08400cb44c03906021a01e42c56c5fdb99037862
Sourabh Kulkarni <sourabhsk.kulkarni@gmail.com>
2024-04-19 09:58:07 +0530

    route setup for get,post,put,delete

--- contact-app-backend/routes/contactRoutes.js
+++ contact-app-backend/routes/contactRoutes.js
@@ -1,2 +1,29 @@
 const express = require("express");
 const router = express.Router();
+
+//GET OCNTACTE ROUTE
+router.route("/").get((req, res) => {
+  res.status(200).json({ message: "Get all contacts" });
+});
+
+//POST OCNTACTE ROUTE
+router.route("/").post((req, res) => {
+  res.status(200).json({ message: "Create contact" });
+});
+
+//GET CONTACT BY ID
+router.route("/:id").get((req, res) => {
+  res.status(200).json({ message: `Get contact for ${req.params.id}`});
+});
+
+//UPDATE OCNTACTE ROUTE
+router.route("/:id").put((req, res) => {
+  res.status(200).json({ message: `Update contact for ${req.params.id}`});
+});
+
+//DELETE OCNTACTE ROUTE
+router.route("/:id").delete((req, res) => {
+  res.status(200).json({ message: `Delete contact for ${req.params.id}`});
+});
+
+module.exports = router;
--- contact-app-backend/server.js
+++ contact-app-backend/server.js
@@ -3,10 +3,12 @@ const dotenv = require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 
-// Basic get route
-app.get("/api/contacts", (req, res) => {
-  res.status(200).json({ message: "Get all contacts" });
-});
+// Basic GET route
+// app.get("/api/contacts", (req, res) => {
+//   res.status(200).json({ message: "Get all contacts" });
+// });
+
+app.use("/api/contacts", require("./routes/contactRoutes"));
 
 app.listen(port, () => {
   console.log(`Server is running on ${port}`);
