8ea82ec33ec3dd18216ef882ba4c33f656d0b801
Sourabh Kulkarni <sourabhsk.kulkarni@gmail.com>
2024-04-19 09:37:22 +0530

    basic get route setup

--- contact-app-backend/server.js
+++ contact-app-backend/server.js
@@ -1,7 +1,12 @@
 const express = require("express");
-const dotenv = require('dotenv').config();
+const dotenv = require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
+
+// Basic get route
+app.get("/api/contacts", (req, res) => {
+  res.status(200).json({ message: "Get all contacts" });
+});
 
 app.listen(port, () => {
   console.log(`Server is running on ${port}`);
