c284051de8e1e991a676c686f0f316111d3fb5e8
Sourabh Kulkarni <sourabhsk.kulkarni@gmail.com>
2024-04-19 11:31:11 +0530

    error handler middleware added

--- contact-app-backend/server.js
+++ contact-app-backend/server.js
@@ -1,8 +1,8 @@
 const express = require("express");
+const errorhandler = require("./middleware/errorHandler");
 const dotenv = require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
-
 // Basic GET route
 // app.get("/api/contacts", (req, res) => {
 //   res.status(200).json({ message: "Get all contacts" });
@@ -10,6 +10,7 @@ const port = process.env.PORT || 5000;
 
 app.use(express.json()); // parser middleware to get req body from client
 app.use("/api/contacts", require("./routes/contactRoutes"));
+app.use(errorhandler); // error handler middlewasre
 
 app.listen(port, () => {
   console.log(`Server is running on ${port}`);
