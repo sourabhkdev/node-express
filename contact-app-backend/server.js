48cae682eb2408fed636283130631d58cd7607ac
Sourabh Kulkarni <sourabhsk.kulkarnin@gmail.com>
2024-04-22 12:26:26 +0530

    connect database from nodejs

--- contact-app-backend/server.js
+++ contact-app-backend/server.js
@@ -1,12 +1,17 @@
+const dotenv = require("dotenv").config();
 const express = require("express");
 const errorhandler = require("./middleware/errorHandler");
-const dotenv = require("dotenv").config();
+const connectDb = require("./config/dbConnection");
+
+connectDb();
 const app = express();
 const port = process.env.PORT || 5000;
 // Basic GET route
 // app.get("/api/contacts", (req, res) => {
 //   res.status(200).json({ message: "Get all contacts" });
 // });
+
+//Connect databse
 
 app.use(express.json()); // parser middleware to get req body from client
 app.use("/api/contacts", require("./routes/contactRoutes"));
